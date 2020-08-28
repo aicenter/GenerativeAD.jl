e26df1e18a916ff0874374393db1564bf0500305
Vít Škvára <vit.skvara@gmail.com>
2020-08-28 15:48:58 +0200

    UCI data loading test

--- test/data.jl
+++ test/data.jl
@@ -1,9 +1,4 @@
 @testset "DATA" begin
-	using Test
-	using GMAD
-	using UCI
-
-
     dataset = "abalone"
 	data, _, _ = UCI.get_loda_data(dataset)
 	cdata = UCI.normalize(hcat(data.normal, data.easy, data.medium))  # this is a control array
@@ -11,7 +6,8 @@
 
 	(tr_x, tr_y), (val_x, val_y), (tst_x, tst_y) = GMAD.load_data(dataset)	
 	@test size(tr_x,1) == size(data.normal,1)
-	@test size(tr_x,2) == floor(Int,size(data.normal,2)*0.6) == length(tr_y) == sum(tr_y)
-	@test size(val_x,2) == floor(Int,size(data.normal,2)*0.2) + floor(Int,na*0.5) == length(val_y) > sum(val_y)
-	@test size(tst_x,2) == floor(Int,size(data.normal,2)*0.2) + floor(Int,na*0.5) == length(tst_y) > sum(tst_y)
+	@test size(tr_x,2) == floor(Int,size(data.normal,2)*0.6) == length(tr_y)
+	@test sum(tr_y) == 0
+	@test size(val_x,2) == floor(Int,size(data.normal,2)*0.2) + floor(Int,na*0.5) == length(val_y) > sum(val_y) > 0
+	@test size(tst_x,2) == floor(Int,size(data.normal,2)*0.2) + floor(Int,na*0.5) == length(tst_y) > sum(tst_y) > 0
 end
