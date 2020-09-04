f038882e263a53c138c6a7d03b977c12904544c8
Vít Škvára <vit.skvara@gmail.com>
2020-09-04 12:48:18 +0200

    named tuples

--- scripts/experiments_tabular/knn.jl
+++ scripts/experiments_tabular/knn.jl
@@ -32,28 +32,19 @@ Should return a Dict that contains a sample of model parameters.
 function sample_params()
 	par_vec = (1:2:101,)
 	argnames = (:k,)
-	return Dict(zip(argnames, map(x->sample(x, 1)[1], par_vec)))
-end
-"""
-	edit_params(data, parameters)
-
-This modifies parameters according to data - only useful for some models.
-"""
-function edit_params(data, parameters)
-	eparams = copy(parameters)
-	return eparams
+	return (;zip(argnames, map(x->sample(x, 1)[1], par_vec))...)
 end
 """
 	fit(data, parameters)
 
 This is the most important function - returns `training_info` and a tuple or a vector of tuples `(score_fun, final_parameters)`.
-`training_info` contains additional information on the training process, the same for all anomaly score functions.
+`training_info` contains additional information on the training process that should be saved, the same for all anomaly score functions.
 Each element of the return vector contains a specific anomaly score function - there can be multiple for each trained model.
 Final parameters is a Dict of names and parameter values that are used for creation of the savefile name.
 """
 function fit(data, parameters)
 	# edit params if needed
-	parameters = edit_params(data, parameters)
+	parameters = GenerativeAD.edit_params(data, parameters)
 
 	# construct model - constructor should only accept kwargs
 	model = GenerativeAD.Models.knn_constructor(;v=:kappa, parameters...)
@@ -63,12 +54,13 @@ function fit(data, parameters)
 		global info, fit_t, _, _, _ = @timed fit!(model, data[1][1])
 	catch e
 		# return an empty array if fit fails so nothing is computed
-		return Dict(:fit_t => NaN), [] 
+		return (fit_t = NaN,), [] 
 	end
 
-	# construct return information 
-	training_info = Dict(
-		:fit_t => fit_t
+	# construct return information - put e.g. the model structure here for generative models
+	training_info = (
+		fit_t = fit_t,
+		model = nothing
 		)
 
 	# now return the different scoring functions
@@ -101,16 +93,11 @@ max_tries = 10
 while try_counter < max_tries 
 	parameters = sample_params()
 	# here, check if a model with the same parameters was already tested
-	if GenerativeAD.check_params(edit_params, savepath, data, parameters)
+	if GenerativeAD.check_params(GenerativeAD.edit_params, savepath, data, parameters)
 		# fit
 		training_info, results = fit(data, parameters)
 		# here define what additional info should be saved together with parameters, scores, labels and predict times
-		save_entries = Dict(
-				:model => modelname,
-				:seed => seed,
-				:dataset => dataset,
-				:fit_t => training_info[:fit_t]
-				)
+		save_entries = merge(training_info, (modelname = modelname, seed = seed, dataset = dataset))
 		
 		# now loop over all anomaly score funs
 		for result in results
--- src/GenerativeAD.jl
+++ src/GenerativeAD.jl
@@ -6,9 +6,11 @@ using Random
 using StatsBase
 using DataDeps
 using MLDatasets
+#using EvalMetrics
 
 include("data.jl")
 include("experiments.jl")
+#include("evaluation.jl")
 include("models/Models.jl")
 
 end #module
