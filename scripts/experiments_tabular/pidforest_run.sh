64eac2e5b05501af866aa8a4d88e82e1dd18e294
Jan Francu <jan.francu@gmail.com>
2020-09-09 13:53:30 +0200

    Modified scripts for slurm.

--- scripts/experiments_tabular/pidforest_run.sh
+++ scripts/experiments_tabular/pidforest_run.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
-# example: `./pidforest_run.sh iris 1`
+# SBATCH --time=24:00:00
+# SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=1
+# SBATCH --mem=20G
+# SBATCH --error=/home/francja5/logs/pidforest.%j.err
+# SBATCH --out=/home/francja5/logs/pidforest.%j.out
+
 MAX_SEED=$1
 DATASET=$2
 
+module load Julia/1.4.1-linux-x86_64
+module load Python/3.8.2-GCCcore-9.3.0
+
+# load virtualenv
+source /home/francja5/pidforest-env/bin/activate
+export PYTHON="/home/francja5/pidforest-env/bin/python"
+
+# PyCall needs to be rebuilt if environment changed
+julia --project -e 'using Pkg; Pkg.build("PyCall"); @info("SETUP DONE")'
+
 for ((SEED=1; SEED<=$MAX_SEED; SEED++))
 do	
-	for ITER in {1..100}
-	do
-		julia pidforest.jl $SEED $DATASET
-	done
+	julia --project ./pidforest.jl $SEED $DATASET
 done
--- scripts/experiments_tabular/run_parallel.sh
+++ scripts/experiments_tabular/run_parallel.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # this runs parallel experiments over all datasets
-MODELSCRIPT=$1
-MAX_SEED=$2
-cat datasets_tabular.txt | xargs -n 1 -P 40 ./${MODELSCRIPT}.sh $MAX_SEED
+MODEL=$1
+NUM_SAMPLES=$2
+MAX_SEED=$3
+
+while read d; do
+    sbatch --array=1-$NUM_SAMPLES -D /home/francja5/Projects/modules/GenerativeAD/scripts/experiments_tabular/ ./${MODEL}_run.sh $MAX_SEED $d
+    # ./${MODEL}_run.sh $MAX_SEED $d
+done < datasets_tabular.txt
