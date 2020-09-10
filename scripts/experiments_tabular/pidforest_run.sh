1b02ae0452f9f264ba6aa81ccd60001f14807b60
Jan Francu <jan.francu@gmail.com>
2020-09-10 10:38:30 +0200

    Deleting whitespace in SBATCH directives.

--- scripts/experiments_tabular/pidforest_run.sh
+++ scripts/experiments_tabular/pidforest_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# SBATCH --time=24:00:00
-# SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=1
-# SBATCH --mem=20G
-# SBATCH --error=/home/francja5/logs/pidforest.%j.err
-# SBATCH --out=/home/francja5/logs/pidforest.%j.out
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=1
+#SBATCH --mem=20G
+#SBATCH --error=/home/francja5/logs/pidforest.%j.err
+#SBATCH --out=/home/francja5/logs/pidforest.%j.out
 
 MAX_SEED=$1
 DATASET=$2
