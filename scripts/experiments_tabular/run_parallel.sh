a10c6d6278b076e7c76742d8966161b9950e61ab
Jan Francu <jan.francu@gmail.com>
2020-09-10 10:43:36 +0200

    Added comments to script args. Added number of concurrent tasks. Removed the chdir (-D) option.

--- scripts/experiments_tabular/run_parallel.sh
+++ scripts/experiments_tabular/run_parallel.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # this runs parallel experiments over all datasets
-MODEL=$1
-NUM_SAMPLES=$2
-MAX_SEED=$3
+MODEL=$1 		# which model to run
+NUM_SAMPLES=$2	# how many repetitions
+MAX_SEED=$3		# how many folds over dataset
+NUM_CONC=$4		# number of concurrent tasks in the array job
 
 while read d; do
-    sbatch --array=1-$NUM_SAMPLES -D /home/francja5/Projects/modules/GenerativeAD/scripts/experiments_tabular/ ./${MODEL}_run.sh $MAX_SEED $d
+    sbatch --array=1-${NUM_SAMPLES}%${NUM_CONC} ./${MODEL}_run.sh $MAX_SEED $d
     # ./${MODEL}_run.sh $MAX_SEED $d
 done < datasets_tabular.txt
