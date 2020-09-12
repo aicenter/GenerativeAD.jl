70d4c36be127bd964a272217ae01019c66c9979a
Jan Francu <jan.francu@gmail.com>
2020-09-12 21:05:45 +0200

    Slurm quality of service set to collaborator. Trying different batch options.

--- scripts/experiments_tabular/pidforest_run.sh
+++ scripts/experiments_tabular/pidforest_run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #SBATCH --time=24:00:00
-#SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=1
-#SBATCH --mem=20G
+#SBATCH --nodes=1 --ntasks-per-node=4 --cpus-per-task=1
+#SBATCH --mem=8G
+#SBATCH --qos==collaborator
 
 MAX_SEED=$1
 DATASET=$2
