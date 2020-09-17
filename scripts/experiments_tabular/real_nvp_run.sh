12ad80995eb6694e101f50a7110b7ea5c40feb9b
Jan Francu <jan.francu@gmail.com>
2020-09-17 08:20:25 +0200

    Added a default python julia environment.

--- scripts/experiments_tabular/real_nvp_run.sh
+++ scripts/experiments_tabular/real_nvp_run.sh
@@ -8,5 +8,10 @@ MAX_SEED=$1
 DATASET=$2
 
 module load Julia/1.4.1-linux-x86_64
+module load Python/3.8.2-GCCcore-9.3.0
+
+# load virtualenv
+source ${HOME}/julia-env/bin/activate
+export PYTHON="${HOME}/julia-env/bin/python"
 
 julia --project ./real_nvp.jl $MAX_SEED $DATASET
