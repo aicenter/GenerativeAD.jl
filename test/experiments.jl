efcd0b853a906ac7de0ae90c18738dad919a6075
Vít Škvára <vit.skvara@gmail.com>
2020-09-25 14:19:11 +0200

    test temp data

--- test/experiments.jl
+++ test/experiments.jl
@@ -4,7 +4,15 @@
 	using Test
 	using GenerativeAD
 
-	tstpath = joinpath(dirname(pathof(GenerativeAD)), "../test/misc")
+	# first create dummy data
+	tstpath = abspath(joinpath(dirname(pathof(GenerativeAD)), "../test/misc"))
+	run(`mkdir $tstpath`)
+	run(`touch $tstpath/model_a=4_b=0.001.bson`)
+	run(`touch $tstpath/a=4_b=0.001.bson`)
+	run(`touch $tstpath/a=4_b=0.001_c=bar.bson`)
+	run(`touch $tstpath/a=4_b=0.001_d=foo.bson`)
+	
+	# now test check_params
 	@test !GenerativeAD.check_params(tstpath, (a=4,))
 	@test !GenerativeAD.check_params(tstpath, (a=4, b=0.001))
 	@test GenerativeAD.check_params(tstpath, (a=4, b=0.002))
@@ -12,4 +20,7 @@
 	@test GenerativeAD.check_params(tstpath, (a=4, b=0.001, c="foo"))
 	@test !GenerativeAD.check_params(tstpath, (a=4, b=0.001, d="foo"))
 	@test GenerativeAD.check_params(tstpath, (a=4, b=0.001, g="a"))
+
+	# cleanup
+	run(`rm -r $tstpath`)
 end
