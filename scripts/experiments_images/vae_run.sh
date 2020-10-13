4a0872d530149d23ee899b093d72dff326ed80d7
Vít Škvára <vit.skvara@gmail.com>
2020-10-13 21:13:20 +0200

    increased training time limit

--- scripts/experiments_images/vae_run.sh
+++ scripts/experiments_images/vae_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --time=24:00:00
-#SBATCH --nodes=1 --ntasks-per-node=2 --cpus-per-task=1
+#SBATCH --time=48:00:00
+#SBATCH --nodes=1 --ntasks-per-node=2 --cpus-per-task=2
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpu
 #SBATCH --mem=80G
