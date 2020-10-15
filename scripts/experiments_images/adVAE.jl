119e1bb2633888e5991b8111c3635a15b943acb3
TheMrGhostman <34610021+TheMrGhostman@users.noreply.github.com>
2020-10-15 10:22:35 +0200

    adVAE

---
+++ scripts/experiments_images/adVAE.jl
@@ -0,0 +1,134 @@
+using DrWatson
+@quickactivate
+using ArgParse
+using GenerativeAD
+using GenerativeAD.Models: anomaly_score, generalized_anomaly_score_gpu
+using BSON
+using StatsBase: fit!, predict, sample
+
+using Flux
+using MLDataPattern
+
+s = ArgParseSettings()
+@add_arg_table! s begin
+   "max_seed"
+		required = true
+		arg_type = Int
+		help = "seed"
+	"dataset"
+		required = true
+		arg_type = String
+		help = "dataset"
+end
+parsed_args = parse_args(ARGS, s)
+@unpack dataset, max_seed = parsed_args
+
+modelname ="adVAE"
+
+function sample_params()
+    argnames = (
+        :hdim, 
+        :nf, 
+        :extra_layers,
+        :gamma,
+        :lambda,
+        :mx,
+        :mz,
+        :lr, 
+        :decay,
+        :batch_size, 
+        :iters, 
+        :check_every, 
+        :patience, 
+        :init_seed,
+    )
+    par_vec = (
+        2 .^(3:8),
+        2 .^(2:7),
+        [0:3 ...],
+        [0.001, 0.003, 0.007, 0.01, 0.03, 0.07, 0.1], # γ
+        [5, 10, 50, 100, 500].^1e-4, # λ  
+        0.5:0.5:2.5, #mx
+        10:10:100, #mz
+        10f0 .^ (-4:-3),
+        0f0:0.1:0.5,
+        2 .^ (5:6),
+        [10000],
+        [10],
+        [30],
+        1:Int(1e8),
+    )
+
+    return NamedTuple{argnames}(map(x->sample(x,1)[1], par_vec))
+end
+
+function fit(data, parameters)
+	# define models (Generator, Discriminator)
+	advae = adVAE(;parameters...)
+
+	# define optimiser
+	try
+		global info, fit_t, _, _, _ = @timed fit!(advae |> gpu, data, parameters)
+	catch e
+		println("Error caught.")
+		return (fit_t = NaN, model = nothing, history = nothing, n_parameters = NaN), []
+	end
+
+	training_info = (
+		fit_t = fit_t,
+		model = info[2]|>cpu,
+		history = info[1], # losses through time
+		npars = info[3], # number of parameters
+		iters = info[4] # optim iterations of model
+		)
+
+    return training_info, 
+    [(x -> GenerativeAD.Models.anomaly_score(advae|>cpu, x; dims=1, L=100), merge(parameters, (L=100, )))]
+    # L = samples for one x in anomaly_score computation
+end
+
+#_________________________________________________________________________________________________
+
+try_counter = 0
+max_tries = 10*max_seed
+
+while try_counter < max_tries
+	parameters = sample_params()
+
+	for seed in 1:max_seed
+		for i in 1:anomaly_classes
+			savepath = datadir("experiments/images/$(modelname)/$(dataset)/ac=$(i)/seed=$(seed)")
+
+			data = GenerativeAD.load_data(dataset, seed=seed, anomaly_class_ind=i)
+			# computing additional parameters
+			in_ch = size(data[1][1],3)
+			isize = maximum([size(data[1][1],1),size(data[1][1],2)])
+
+			isize = (isize % 16 != 0) ? isize + 16 - isize % 16 : isize
+			# update parameter
+			parameters = merge(parameters, (isize=isize, in_ch = in_ch, out_ch = 1))
+			# here, check if a model with the same parameters was already tested
+			if GenerativeAD.check_params(savepath, parameters)
+
+				data = GenerativeAD.Models.preprocess_images(data, parameters)
+				#(X_train,_), (X_val, y_val), (X_test, y_test) = data
+				training_info, results = fit(data, parameters)
+				# saving model separately
+				if training_info.model !== nothing
+					tagsave(joinpath(savepath, savename("model", parameters, "bson")), Dict("model"=>training_info.model), safe = true)
+					training_info = merge(training_info, (model = nothing,))
+				end
+				save_entries = merge(training_info, (modelname = modelname, seed = seed, dataset = dataset, anomaly_class = i))
+				# now loop over all anomaly score funs
+				for result in results
+					GenerativeAD.experiment(result..., data, savepath; save_entries...)
+				end
+				global try_counter = max_tries + 1
+			else
+				@info "Model already present, sampling new hyperparameters..."
+				global try_counter += 1
+			end
+		end
+	end
+end
+(try_counter == max_tries) ? (@info "Reached $(max_tries) tries, giving up.") : nothing
---
+++ src/models/adVAE.jl
@@ -0,0 +1,187 @@
+struct adVAE
+    encoder
+    generator
+    transformer
+end
+
+function adVAE(;idim::Int=1, zdim::Int=1, hdim::Int=64, activation = "relu", nlayers::Int=3, kwargs...)
+    encoder = Flux.Chain(
+        build_mlp(idim, hdim, hdim, nlayers-1, activation=activation),
+        ConditionalDists.SplitLayer(hdim, [zdim,zdim], [identity,softplus])
+    )
+    transformer = Flux.Chain(
+        build_mlp(zdim*2, hdim, hdim, 2, activation=activation),
+        ConditionalDists.SplitLayer(hdim, [zdim,zdim], [identity,softplus])
+    )
+    generator = build_mlp(zdim, hdim, idim, nlayers, activation=activation, lastlayer="linear")
+    
+    return adVAE(encoder, generator, transformer)
+end
+
+function Conv_adVAE(
+        ;isize::Int=1, 
+        in_ch::Int=1, 
+        zdim::Int=64, 
+        nf::Int=16, 
+        extra_layers::Int=0, 
+        hdim::Int=64,
+        depth::Int=3, 
+        activation="relu", 
+        kwargs...
+        )
+
+    encoder = Flux.Chain(
+        ConvEncoder(isize, in_ch, zdim, nf, extra_layers),
+        x->reshape(x, (1,size(x)[end])), # equivalent to flatten
+        ConditionalDists.SplitLayer(zdim, [zdim, zdim], [identity, softplus])
+    )
+    transfromer = Flux.Chain(
+        build_mlp(zdim*2, hdim, hdim, depth, activation=activation),
+        ConditionalDists.SplitLayer(hdim, [zdim,zdim], [identity,softplus])
+    )
+    generator = Flux.Chain(
+        Flux.Dense(zdim, zdim, activation=activation),
+        x->reshape(x, (1,1,1,size(x)[end])), # inverse to flatten
+        ConvDecoder(isize, 1, in_ch, nf, extra_layers) 
+    )
+
+    return adVAE(encoder, generator, transformer)
+end
+
+Flux.@functor adVAE
+
+function (advae::adVAE)(x)
+    μ, Σ = advae.encoder(x)
+    z = μ + Σ * randn(Float32)
+    xᵣ = advae.generator(z)
+end
+
+kl_divergence(μ, Σ) = 0.5 * sum(1 .+ log.(Σ.^2) - μ.^2  - Σ.^2) # actually it is -kl_divergence
+kl_divergence(μ₁, Σ₁, μ₂, Σ₂) = sum(log.(Σ₂ ./ Σ₁) + (Σ₁.^2 + (μ₁ - μ₂).^2) ./ (2*Σ₂.^2) .- 0.5)
+
+function loss(advae::adVAE, x; γ=1e-3, λ=1e-2, mx=1, mz=1)
+    μ, Σ = advae.encoder(x)
+    μₜ, Σₜ = advae.transformer(cat(μ, Σ, dims=1))
+    
+    z = μ + Σ * randn(Float32)
+    zₜ = μₜ + Σₜ * randn(Float32)
+    
+    xᵣ = advae.generator(z)
+    xₜᵣ = advae.generator(zₜ)
+    
+    μᵣ, Σᵣ = advae.encoder(xᵣ)
+    μₜᵣ, Σₜᵣ = advae.encoder(xₜᵣ)
+
+    # 𝓛 for generator => 𝓛 = 𝓛_z + 𝓛_zₜ 
+    𝓛_z = Flux.Losses.mse(x, xᵣ) .+ γ * kl_divergence(μᵣ, Σᵣ)
+    𝓛_zₜ = max(0, (mx - Flux.Losses.mse(xᵣ, xₜᵣ))) + γ * max(0, (mz - kl_divergence(μₜᵣ, Σₜᵣ)))
+    𝓛 = 𝓛_z + 𝓛_zₜ
+    # 𝓛ₜ for transformer 
+    𝓛ₜ = kl_divergence(μ, Σ, μₜ, Σₜ)
+    # 𝓛ₑ for encoder
+    𝓛ₑ = Flux.Losses.mse(x, xᵣ) .+ γ * kl_divergence(μ, Σ) 
+        + γ * max(0, (mz - kl_divergence(μᵣ, Σᵣ)))
+        + γ * max(0, (mz - kl_divergence(μₜᵣ, Σₜᵣ)))
+
+    return 𝓛 + λ*𝓛ₜ, 𝓛ₑ, 𝓛, 𝓛ₜ
+end
+
+
+function anomaly_score(advae::adVAE, real_input; L=100, dims=3, batch_size=64)
+    real_input = Flux.Data.DataLoader(real_input, batchsize=batch_size)
+    testmode!(advae)
+    advae = advae |> gpu
+    output = Array{Float32}([])
+    for x in real_input
+        X = Array{Float32}(undef, size(x)[end], 1)
+        μ, Σ = advae.encoder(x)
+        for l=1:L
+            z = μ + Σ * randn(Float32)
+            xᵣ = advae.generator(z)
+            rec_loss = vec(Flux.mse(latent_i, latent_o, agg=x->mean(x, dims=dims))) |> cpu # loss per batch
+            X = cat(X, rec_loss, dims=2)
+        end
+        output = cat(output, Flux.mean(X, dims=2), dims=1)
+    end
+    testmode!(advae, false)
+    return output
+end
+
+
+function StatsBase.fit!(advae::adVAE, data, params)
+	# prepare batches & loaders
+	train_loader, val_loader = prepare_dataloaders(data, params)
+	# training info logger
+	history = MVHistory()
+	# prepare for early stopping
+	best_adVAE = deepcopy(advae)
+	patience = params.patience
+	best_val_loss = 1e10
+	val_batches = length(val_loader)
+
+	# ADAMW(η = 0.001, β = (0.9, 0.999), decay = 0) = Optimiser(ADAM(η, β), WeightDecay(decay))
+	opt = haskey(params, :decay) ? ADAMW(params.lr, (0.9, 0.999), params.decay) : ADAM(params.lr)
+
+	ps_step1 = Flux.params(advae.generator, advae.transformer)
+	ps_step2 = Flux.params(advae.encoder)
+
+
+	progress = Progress(length(train_loader))
+	for (iter, X) in enumerate(train_loader)
+		# training step 1
+		loss_1, back = Flux.pullback(ps_step1) do
+            loss(advae|>gpu , getobs(X)|>gpu, γ=params.gamma, λ=params.lambda, mx=params.mx, mz=params.mz)
+		end
+		grad_step1 = back((1f0, 0f0, 0f0, 0f0))
+        Flux.Optimise.update!(opt, ps_step1, grad_step1)
+        # training step 2
+        loss_2, back = Flux.pullback(ps_step2) do
+            loss(advae|>gpu , getobs(X)|>gpu, γ=params.gamma, λ=params.lambda, mx=params.mx, mz=params.mz)
+		end
+        grad_step2 = back((0f0, 1f0, 0f0, 0f0))
+		Flux.Optimise.update!(opt, ps_step2, grad_step2)
+
+
+        push!(history, :loss_step_1, iter, loss_1[1])
+        push!(history, :encoder_loss, iter, loss_2[2])
+        push!(history, :generator_loss, iter, loss_1[3])
+        push!(history, :transformer_loss, iter, loss_1[4])
+
+		next!(progress; showvalues=[
+			(:iters, "$(iter)/$(params.iters)"),
+			(:loss_step_1, loss_1[1]),
+			(:loss_step_2, loss_2[2])
+			])
+        # TODO: check and prepare early stopping 
+        if mod(iter, params.check_every) == 0
+            total_val_loss = 0
+            testmode!(advae)
+            for X_val in val_loader
+                X_val = X_val |> gpu
+                μ, Σ = advae.encoder(X_val)
+                z = μ + Σ * randn(Float32)
+                xᵣ = advae.generator(z)
+                total_val_loss += Flux.mse(X_val, xᵣ)
+            end
+            testmode!(advae, false)
+            push!(history, :validation_loss, iter, total_val_loss/val_batches)
+
+			if total_val_loss < best_val_loss
+				best_val_loss = total_val_loss
+				patience = params.patience
+				best_adVAE = deepcopy(advae)
+			else
+				patience -= 1
+				if patience == 0
+					@info "Stopped training after $(iter) iterations"
+					global iters = iter - params.check_every*params.patience
+					break
+				end
+			end
+		end
+		if iter == params.iters
+			global iters = params.iters
+		end
+	end
+	return history, best_adVAE, sum(map(p->length(p), ps_step1))+sum(map(p->length(p), ps_step2)), iters
+end
