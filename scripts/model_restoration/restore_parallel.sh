9d7a7ea4125258e726b0cef9c51634c6ffde20bc
Vít Škvára <vit.skvara@gmail.com>
2020-10-26 13:49:08 +0100

    model restoration

---
+++ scripts/model_restoration/restore_parallel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This regenerates the modelfiles for model and specific data type
+# Run from this folder only.
+MODEL=$1 		# which model to run
+DATA=$2		    # tabular/images
+DATASET_FILE=$3	# file with dataset list
+
+LOG_DIR="${HOME}/logs/${MODEL}_rest"
+
+if [ ! -d "$LOG_DIR" ]; then
+	mkdir $LOG_DIR
+fi
+
+while read d; do
+	# submit to slurm
+    sbatch \
+    --output="${LOG_DIR}/${d}-%A.out" \
+	./restore_modelfiles.sh /home/skvarvit/generativead/GenerativeAD.jl/data/experiments/${DATA}/${MODEL}/${d}
+done < ${DATASET_FILE}
