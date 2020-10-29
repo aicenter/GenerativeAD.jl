029330f0dddaeb0303fa3c9e3e60d5b5eb79cba7
Jan Francu <jan.francu@gmail.com>
2020-10-29 07:04:31 +0100

    Added cmd argument for running eval for img or tab data.

--- scripts/evaluation/run_eval.sh
+++ scripts/evaluation/run_eval.sh
@@ -6,14 +6,18 @@
 #SBATCH --mem-per-cpu 8G
 #SBATCH --time 4:00:00
 #SBATCH --job-name generativead-eval
-#SBATCH --output eval-%J.out
 #SBATCH --qos==collaborator
 
-module load Julia/1.5.1-linux-x86_64
-module load Python/3.8.2-GCCcore-9.3.0
+DATASETS=$1
+if [ "$DATASETS" == "tabular" ] || [ "$DATASETS" == "images" ]; then
+    module load Julia/1.5.1-linux-x86_64
+    module load Python/3.8.2-GCCcore-9.3.0
 
-source ${HOME}/julia-env/bin/activate
-export PYTHON="${HOME}/julia-env/bin/python"
+    source ${HOME}/julia-env/bin/activate
+    export PYTHON="${HOME}/julia-env/bin/python"
 
-julia --threads 32 --project ./generate_stats.jl experiments/images evaluation/images 
-julia --threads 32 --project ./collect_stats.jl  evaluation/images evaluation/images_eval.bson -f
+    julia --threads 32 --project ./generate_stats.jl experiments/${DATASETS} evaluation/${DATASETS} 
+    julia --threads 32 --project ./collect_stats.jl  evaluation/${DATASETS} evaluation/${DATASETS}_eval.bson -f
+else
+    echo "Unsupported dataset type. Only 'images' or 'tabular' are supported."
+fi
