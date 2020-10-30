06b03b2b8153ef20064f4c55474e60fc59662c7c
TheMrGhostman <34610021+TheMrGhostman@users.noreply.github.com>
2020-10-30 15:12:19 +0100

    fixes to code
    - stable version. all funcitons work togeter.
    - TODO: fix scripts / calling of functions

--- src/models/utils/utils.jl
+++ src/models/utils/utils.jl
@@ -1,5 +1,15 @@
+using DrWatson
 using DataFrames
 using CSV
+using BSON
+using ValueHistories
+# because of vae
+using GenerativeAD
+using ConditionalDists
+using GenerativeModels
+import GenerativeModels: VAE
+using Distributions
+using DistributionsAD
 
 """
 	function resize_images(images, isize::Int, channels=1)
@@ -126,41 +136,42 @@ function return_best_n(df, n=10, dataset="MNIST")
 end
 
 
-function load_encoding(model="vae_img", data; dataset::String="MNIST", anomaly_class::Int=1, seed::Int=1, model_index::Int=1)
-    # load csv
-	df = CSV.read(datadir("$(model)_tab.csv")) 
-	df = return_best_n(df, 10, dataset)
-    # checking if model configuration was trained on all classes and all seeds
-    n_comb = 500
-    check_comb = sum([sum(df.ind .== i) for i=1:10])
-    if check_comb < n_comb
-        @info "One of chosen models does not include all anomaly classes and all seeds!! $(check_comb) out of $(n_comb)"
-    end
-    # get correct model path
-    encoding_path = df[(df.ac .== anomaly_class) .& (df.seed .==1) .& (df.ind .==model_index),:][:path]
-    # load model and encodings
-	model = BSON.load(encoding_path)
-	data = (model[:tr_encodings], data[1][2]), (model[:val_encodings], data[2][2]), ( model[:test_encodings], data[3][2])
-
-    return data, split(encoding_path, "/")[end]
-end
-
 function load_encoding(model="vae_tabular", data; dataset::String="iris", seed::Int=1, model_index::Int=1)
     # load csv
 	df = CSV.read(datadir("$(model)_tab.csv")) 
 	df = return_best_n(df, 10, dataset)
     # checking if model configuration was trained on all classes and all seeds
+    n_comb = 50
+    check_comb = sum([sum(df.ind .== i) for i=1:10])
+    if check_comb < n_comb
+        @info "One of chosen models does not include all seeds!! $(check_comb) out of $(n_comb)"
+    end
+    # get correct model path
+    encoding_path = df[(df.seed .==1) .& (df.ind .==model_index),:][:path][1]
+    # load model and encode data
+	model = BSON.load(encoding_path)
+	encodings = map(x->cpu(GenerativeAD.Models.encode_mean(model["model"], x)), (data[1][1], data[2][1], data[3][1]))
+	data = (encodings[1], data[1][2]), (encodings[2], data[2][2]), (encodings[3], data[3][2])
+
+    return data, split(encoding_path, "/")[end]
+end
+
+
+function load_encoding(model="vae_images", data, anomaly_class; dataset::String="MNIST", seed::Int=1, model_index::Int=1)
+    # load csv
+	df = CSV.read(datadir("$(model)_tab.csv")) 
+	df = return_best_n(df, 10, dataset)
+    # checking if model configuration was trained on all classes and all seeds
     n_comb = 500
     check_comb = sum([sum(df.ind .== i) for i=1:10])
     if check_comb < n_comb
         @info "One of chosen models does not include all anomaly classes and all seeds!! $(check_comb) out of $(n_comb)"
     end
     # get correct model path
-    encoding_path = df[(df.seed .==1) .& (df.ind .==model_index),:][:path]
-    # load model and encode data
+    encoding_path = df[(df.ac .== anomaly_class) .& (df.seed .==1) .& (df.ind .==model_index),:][:path][1]
+    # load model and encodings
 	model = BSON.load(encoding_path)
-	encodings = map(x->cpu(GenerativeAD.Models.encode_mean(model[:model], x)), (data[1][1], data[2][1], data[3][1]))
-	data = (encodings[1], data[1][2]), (encodings[2], data[2][2]), (encodings[3], data[3][2])
+	data = (model["tr_encodings"], data[1][2]), (model["val_encodings"], data[2][2]), ( model["tst_encodings"], data[3][2])
 
     return data, split(encoding_path, "/")[end]
 end
