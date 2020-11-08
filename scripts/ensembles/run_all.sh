1b2250d13856db008673b3ebc4f2357f6ad2b392
Jan Francu <jan.francu@gmail.com>
2020-11-08 22:27:41 +0100

    Added simple run all script.

---
+++ scripts/ensembles/run_all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+DATASET_TYPE=$1
+
+if [ "$DATASET_TYPE" == "tabular" ]; then
+    models=("GANomaly" "MAF" "MO_GAAL" "RealNVP" "aae" "aae_vamp" "abod" "adVAE" "fmgan" "gan" "hbos" "if" "knn" "loda" "lof" "ocsvm" "pidforest" "sptn" "vae" "wae" "wae_vamp")
+
+    for m in ${models[*]}
+    do
+        ./run_ensemble_parallel.sh ${m} tabular ../experiments_tabular/datasets_tabular.txt
+    done
+elif  [ "$DATASET_TYPE" == "images" ]; then
+    models=("Conv-GANomaly" "Conv-SkipGANomaly" "aae" "fmgan" "knn" "ocsvm" "vae" "wae")
+
+    for m in ${models[*]}
+    do
+        ./run_ensemble_parallel.sh ${m} images ../experiments_images/datasets_images.txt
+    done
+else
+    echo "Unsupported dataset type. Only 'images' or 'tabular' are supported."
+fi
