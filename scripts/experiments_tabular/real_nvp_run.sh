38b4c95073e49c741952a4c457de67816ecfd57b
Jan Francu <jan.francu@gmail.com>
2020-11-12 06:50:33 +0100

    Increased flexibility of flows.

--- scripts/experiments_tabular/real_nvp_run.sh
+++ scripts/experiments_tabular/real_nvp_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1 --ntasks-per-node=4 --cpus-per-task=1
-#SBATCH --mem=16G
+#SBATCH --mem=24G
 #SBATCH --qos==collaborator
 
 MAX_SEED=$1
@@ -10,8 +10,6 @@ DATASET=$2
 module load Julia/1.5.1-linux-x86_64
 module load Python/3.8.2-GCCcore-9.3.0
 
-# load virtualenv
-source ${HOME}/julia-env/bin/activate
-export PYTHON="${HOME}/julia-env/bin/python"
+julia --project -e 'using Pkg; Pkg.instantiate();'
 
 julia --project ./real_nvp.jl $MAX_SEED $DATASET
