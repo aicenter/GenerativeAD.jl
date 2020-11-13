a29bdb12956e81cfd0dfc7105a5e118afc7e23a0
Vít Škvára <vit.skvara@gmail.com>
2020-11-13 13:20:00 +0100

    sample score

---
+++ scripts/sample_score/sample_score_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1 --ntasks-per-node=2 --cpus-per-task=1
+#SBATCH --mem=20G
+
+MODEL=$1
+DATATYPE=$2
+DATASET=$3
+SEED=$4
+AC=$5
+
+module load Julia/1.5.1-linux-x86_64
+module load Python/3.8.2-GCCcore-9.3.0
+
+julia ./sample_score.jl $MODEL $DATATYPE $DATASET $SEED $AC
+
