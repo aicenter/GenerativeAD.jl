ecf2d9b72bfd5b2fdee54a7c57a659bd281d45d3
Jan Francu <jan.francu@gmail.com>
2020-11-15 21:42:15 +0100

    Added suffix to run ensemble evaluation.

--- scripts/evaluation/run_eval.sh
+++ scripts/evaluation/run_eval.sh
@@ -9,12 +9,13 @@
 #SBATCH --qos==collaborator
 
 DATASETS=$1
+SUFFIX=$2
 if [ "$DATASETS" == "tabular" ] || [ "$DATASETS" == "images" ]; then
     module load Julia/1.5.1-linux-x86_64
     module load Python/3.8.2-GCCcore-9.3.0
 
-    julia --threads 32 --project ./generate_stats.jl experiments/${DATASETS} evaluation/${DATASETS} 
-    julia --threads 32 --project ./collect_stats.jl  evaluation/${DATASETS} evaluation/${DATASETS}_eval.bson -f
+    julia --threads 32 --project ./generate_stats.jl experiments${SUFFIX}/${DATASETS} evaluation${SUFFIX}/${DATASETS} 
+    julia --threads 32 --project ./collect_stats.jl  evaluation${SUFFIX}/${DATASETS} evaluation${SUFFIX}/${DATASETS}_eval.bson -f
 else
     echo "Unsupported dataset type. Only 'images' or 'tabular' are supported."
 fi
