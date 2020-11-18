37530e5352f7cd3ac4853f9590a91d78fd8a4d92
Jan Francu <jan.francu@gmail.com>
2020-11-18 20:42:49 +0100

    Increased requirements for evaluation.

--- scripts/evaluation/run_eval.sh
+++ scripts/evaluation/run_eval.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --partition cpufast
+#SBATCH --partition cpu
 #SBATCH --nodes 1
-#SBATCH --cpus-per-task 32
+#SBATCH --cpus-per-task 48
 #SBATCH --ntasks-per-node 1
-#SBATCH --mem-per-cpu 8G
-#SBATCH --time 4:00:00
+#SBATCH --mem-per-cpu 16G
+#SBATCH --time 24:00:00
 #SBATCH --job-name generativead-eval
 #SBATCH --qos==collaborator
 
@@ -14,8 +14,8 @@ if [ "$DATASETS" == "tabular" ] || [ "$DATASETS" == "images" ]; then
     module load Julia/1.5.1-linux-x86_64
     module load Python/3.8.2-GCCcore-9.3.0
 
-    julia --threads 32 --project ./generate_stats.jl experiments${SUFFIX}/${DATASETS} evaluation${SUFFIX}/${DATASETS} 
-    julia --threads 32 --project ./collect_stats.jl  evaluation${SUFFIX}/${DATASETS} evaluation${SUFFIX}/${DATASETS}_eval.bson -f
+    julia --threads 48 --project ./generate_stats.jl experiments${SUFFIX}/${DATASETS} evaluation${SUFFIX}/${DATASETS}
+    julia --threads 48 --project ./collect_stats.jl  evaluation${SUFFIX}/${DATASETS} evaluation${SUFFIX}/${DATASETS}_eval.bson -f
 else
     echo "Unsupported dataset type. Only 'images' or 'tabular' are supported."
 fi
