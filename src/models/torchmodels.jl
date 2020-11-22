13080aaec5764fa5a1362d15fc735a1ef41d05e0
TheMrGhostman <ghostmanthe@gmail.com>
2020-11-22 12:21:08 +0100

    Revert "torchmodel fixed"
    This reverts commit 8149c047820af51ee8ec57544f4b192d2f28e1f6.

--- src/models/torchmodels.jl
+++ src/models/torchmodels.jl
@@ -1,22 +1,19 @@
 using PyCall
 using StatsBase
 
-pushfirst!(PyVector(pyimport("sys")["path"]), "")
-#ushfirst!(PyVector(pyimport("sys")["path"]), @__DIR__)
-
 abstract type TorchModel end
 
-"""
 function StatsBase.fit!(model::TorchModel, X::Array{T, 2}, max_iters=10000, lr_gan=1e-4, lr_enc=1e-4, 
-    	batch_size=64, n_critic=5) where T<:Real
+    batch_size=64, n_critic=5) where T<:Real
 	# transposition since Python models are row major
-    	model.model.fit(Array(transpose(X)), max_iters, lr_gan, lr_enc, batch_size, n_critic)
+    model.model.fit(Array(transpose(X)), max_iters, lr_gan, lr_enc, batch_size, n_critic)
 end
 
 function StatsBase.predict(model::TorchModel, X::Array{T, 2}) where T<:Real
 	model.model.predict(Array(transpose(X)))
 end
 
+"""
 	fAnoGAN_GP(;idim=(1,2,2), zdim=100, kernelsizes=(9,7,5,3), channels=(128,64,32,16), scalings=(2,2,2,1), 
         activation="relu", batchnorm=false, usegpu=true))
 
@@ -24,17 +21,16 @@ The fAnoGAN model with gradient penalization.
 """
 mutable struct fAnoGAN_GP <: TorchModel
 	model
+
+    function fAnoGAN_GP(;kwargs...)
+        pushfirst!(PyVector(pyimport("sys")["path"]), "")
+		py"""
+		import fanogan_gp
+        import torch
+
+		def construct_fanogan_gp(kwargs):
+			return fanogan_gp.fAnoGAN_GP(**kwargs)
+		"""
+		new(py"construct_fanogan_gp"(kwargs))
+	end
 end
-
-function fAnoGAN_GP(;kwargs...)
-py"""
-import numpy
-import torch
-import fanogan_gp
-
-def c_fanogan_gp(kwargs):
-	return fanogan_gp.fAnoGAN(**kwargs)
-"""
-	return fAnoGAN_GP(py"c_fanogan_gp"(kwargs))
-end
-
