2948c9ef614761549230c4d548a4dc6ea01f91e6
TheMrGhostman <ghostmanthe@gmail.com>
2020-11-22 12:47:20 +0100

    Update torchmodels.jl

--- src/models/torchmodels.jl
+++ src/models/torchmodels.jl
@@ -6,7 +6,7 @@ pushfirst!(PyVector(pyimport("sys")["path"]), "")
 
 abstract type TorchModel end
 
-"""
+
 function StatsBase.fit!(model::TorchModel, X::Array{T, 2}, max_iters=10000, lr_gan=1e-4, lr_enc=1e-4, 
 		batch_size=64, n_critic=5) where T<:Real
 	# transposition since Python models are row major
@@ -16,7 +16,7 @@ end
 function StatsBase.predict(model::TorchModel, X::Array{T, 2}) where T<:Real
 	model.model.predict(Array(transpose(X)))
 end
-
+"""
 	fAnoGAN_GP(;idim=(1,2,2), zdim=100, kernelsizes=(9,7,5,3), channels=(128,64,32,16), scalings=(2,2,2,1), 
 		activation="relu", batchnorm=false, usegpu=true))
 
