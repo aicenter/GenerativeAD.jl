37af60b16ca0338c2a25b0ed4ba538eeb392880a
TheMrGhostman <34610021+TheMrGhostman@users.noreply.github.com>
2020-11-23 18:03:28 +0100

    run_parallel

--- scripts/experiments_images/vae_run.sh
+++ scripts/experiments_images/vae_run.sh
@@ -8,8 +8,9 @@
 MAX_SEED=$1
 DATASET=$2
 ANOMALY_CLASSES=$3
+METHOD=$4
 
 module load Julia/1.5.1-linux-x86_64
 module load Python/3.8.2-GCCcore-9.3.0
 
-julia ./vae.jl ${MAX_SEED} $DATASET ${ANOMALY_CLASSES}
+julia ./vae.jl ${MAX_SEED} $DATASET ${ANOMALY_CLASSES} $METHOD
