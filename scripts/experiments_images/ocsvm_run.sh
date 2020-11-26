2ecd8e0bb7b6eb474d50b4f4942be54845e88c57
TheMrGhostman <ghostmanthe@gmail.com>
2020-11-26 16:38:31 +0100

    ocsvm loi

--- scripts/experiments_images/ocsvm_run.sh
+++ scripts/experiments_images/ocsvm_run.sh
@@ -6,6 +6,7 @@
 MAX_SEED=$1
 DATASET=$2
 ANOMALY_CLASSES=$3
+METHOD=$4
 
 module load Julia/1.5.1-linux-x86_64
 module load Python/3.8.2-GCCcore-9.3.0
@@ -17,4 +18,4 @@ export PYTHON="${HOME}/sklearn-env/bin/python"
 # PyCall needs to be rebuilt if environment changed
 julia --project -e 'using Pkg; Pkg.build("PyCall"); @info("SETUP DONE")'
 
-julia ./ocsvm.jl ${MAX_SEED} $DATASET ${ANOMALY_CLASSES}
+julia ./ocsvm.jl ${MAX_SEED} $DATASET ${ANOMALY_CLASSES} $METHOD
