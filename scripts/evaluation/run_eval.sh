b74b47073eedcfc43e33cceffcf0482e70121396
Jan Francu <jan.francu@gmail.com>
2021-03-31 14:42:17 +0200

    Pkg and julia version bump.

--- scripts/evaluation/run_eval.sh
+++ scripts/evaluation/run_eval.sh
@@ -11,7 +11,7 @@
 DATASETS=$1
 SUFFIX=$2
 if [ "$DATASETS" == "tabular" ] || [ "$DATASETS" == "images" ] || [ "$DATASETS" == "images_leave-one-in" ]; then
-    module load Julia/1.5.1-linux-x86_64
+    module load Julia/1.5.3-linux-x86_64
     module load Python/3.8.2-GCCcore-9.3.0
 
     julia --threads 48 --project ./generate_stats.jl experiments${SUFFIX}/${DATASETS} evaluation${SUFFIX}/${DATASETS}
