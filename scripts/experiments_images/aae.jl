52740aa67955899d4703a6fc9dde83970ee472a4
vitskvara <vit.skvara@gmail.com>
2021-05-11 08:48:13 +0200

    aae working

--- scripts/experiments_images/aae.jl
+++ scripts/experiments_images/aae.jl
@@ -38,8 +38,6 @@ parsed_args = parse_args(ARGS, s)
 ################ THIS PART IS TO BE PROVIDED FOR EACH MODEL SEPARATELY ################
 modelname = "aae"
 
-DrWatson.projectdir() = "/home/skvarvit/generativead/GenerativeAD.jl"
-
 # sample parameters, should return a Dict of model kwargs 
 """
 	sample_params()
