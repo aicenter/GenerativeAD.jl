a7402fe669b0f68b65cb808ef83bea01812132e4
Vít Škvára <vit.skvara@gmail.com>
2021-05-24 11:38:15 +0200

    ganomaly data editing

--- scripts/loi_eval_fix/SkipGANomaly.jl
+++ scripts/loi_eval_fix/SkipGANomaly.jl
@@ -40,4 +40,56 @@ end
 ##################
 # this is common #
 ##################
-include("run_loop.jl")
+seed = 1
+method = "leave-one-in"
+contamination = 0.0
+
+main_inpath = datadir("experiments/images_leave-one-in_backup/$(modelname)/$(dataset)")
+main_savepath = datadir("experiments/images_leave-one-in/$(modelname)/$(dataset)")
+mkpath(main_savepath)
+
+# this loop unfortunately cannot be in a function, since loading of bson is only safe ot top level
+for ac in 1:10
+	data = GenerativeAD.load_data(dataset, seed=seed, anomaly_class_ind=ac, method=method, 
+		contamination=contamination)
+	in_ch = size(data[1][1],3)
+	isize = maximum([size(data[1][1],1),size(data[1][1],2)])
+	isize = (isize % 16 != 0) ? isize + 16 - isize % 16 : isize
+	# update parameter
+	data = GenerativeAD.Models.preprocess_images(data, parameters)
+
+	inpath = joinpath(main_inpath, "ac=$ac/seed=$seed")
+	savepath = joinpath(main_savepath, "ac=$ac/seed=$seed")
+	mkpath(savepath)
+	fs = readdir(inpath, join=true)
+	sfs = filter(x->!(occursin("model", x)), fs)
+	mfs = filter(x->(occursin("model", x)), fs)
+
+	@info "Loaded $(length(mfs)) modelfiles in $inpath, processing..."
+	for mf in mfs
+		# load the bson file on top level, otherwise we get world age problems
+		model_data = load(mf)
+		if haskey(model_data, "parameters")
+			parameters = model_data["parameters"]
+		else # this is in case parameters are not saved in the model file
+			init_seed = DrWatson.parse_savename(mf)[2]["init_seed"]
+			sf = sfs[map(x->DrWatson.parse_savename(x)[2]["init_seed"], sfs) .== init_seed][1]
+			score_data = load(sf)
+			parameters = score_data[:parameters]
+		end
+		parameters = merge(parameters, (isize=isize, in_ch = in_ch, out_ch = 1))
+		try
+			training_info, results = evaluate(model_data, data, parameters) # this produces parameters, encodings, score funs
+			save_results(parameters, training_info, results, savepath, data, 
+				ac, modelname, seed, dataset, contamination) # this computes and saves score and model files
+		catch e
+			if isa(e, LoadError)
+				@warn "$mf failed during result evaluation due to $e"
+			else
+				rethrow(e)
+			end
+		end
+	end
+end
+
+
