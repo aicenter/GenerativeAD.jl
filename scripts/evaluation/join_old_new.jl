acce1ded190b7d319433af3d4f4acd31abeb9947
vitskvara <vit.skvara@gmail.com>
2022-02-15 14:03:45 +0100

    join old new

--- scripts/evaluation/join_old_new.jl
+++ scripts/evaluation/join_old_new.jl
@@ -1,20 +1,21 @@
 using FileIO, BSON, DataFrames
+using ArgParse
+using DrWatson
+@quickactivate
 
-f_new = "/home/skvarvit/generativead-sgad/GenerativeAD.jl/data/evaluation/images_leave-one-in_eval.bson"
-f_old = "/home/skvarvit/generativead/GenerativeAD.jl/data/evaluation/images_leave-one-in_eval.bson"
-f_target = "/home/skvarvit/generativead-sgad/GenerativeAD.jl/data/evaluation/images_leave-one-in_eval_all.bson"
-
-d_new = load(f_new)
-d_old = load(f_old)
-
-df = vcat(d_old[:df], d_new[:df])
-
-save(f_target, Dict(:df => df))
-
-# now repeat for mvtec data
-f_new = "/home/skvarvit/generativead-sgad/GenerativeAD.jl/data/evaluation/images_mvtec_eval.bson"
-f_old = "/home/skvarvit/generativead/GenerativeAD.jl/data/evaluation/images_mvtec_eval.bson"
-f_target = "/home/skvarvit/generativead-sgad/GenerativeAD.jl/data/evaluation/images_mvtec_eval_all.bson"
+s = ArgParseSettings()
+@add_arg_table s begin
+    "fnew"
+        help = "the new eval bson"
+    "fold"
+        help = "the old eval bson"
+    "fold"
+        help = "the target eval bson"   
+end
+parsed_args = parse_args(ARGS, s)
+f_new = datadir("evaluation/$(parsed_args["fnew"])")
+f_old = "/home/skvarvit/generativead/GenerativeAD.jl/data/evaluation/$(parsed_args["fold"])"
+f_target = datadir("evaluation/$(parsed_args["ftarget"])")
 
 d_new = load(f_new)
 d_old = load(f_old)
