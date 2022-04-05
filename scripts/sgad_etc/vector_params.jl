12df7d7534436c819b9adb8ebfd23a9e9b0d8ee5
Vít Škvára <vit.skvara@gmail.com>
2022-04-05 13:34:00 +0200

    vector params in mvtec

--- scripts/sgad_etc/vector_params.jl
+++ scripts/sgad_etc/vector_params.jl
@@ -3,10 +3,6 @@ using DrWatson
 using GenerativeAD
 using BSON, FileIO, DataFrames
 
-model = "sgvae"
-datatype = "leave-one-in"
-mainpath = datadir("experiments/images_$(datatype)/$(model)")
-
 function row(d)
     ps = d[:parameters]
     return  [
@@ -19,6 +15,10 @@ function row(d)
         ps[:detach_mask]
         ]
 end
+
+model = "sgvae"
+datatype = "leave-one-in"
+mainpath = datadir("experiments/images_$(datatype)/$(model)")
 
 outdf = DataFrame(
     :modelname => [],
@@ -34,6 +34,40 @@ outdf = DataFrame(
 for dataset in ["wildlife_MNIST", "CIFAR10", "SVHN2"]
     for ac in 1:10
         for seed in 1:1
+            inpath = joinpath(mainpath, "$(dataset)/ac=$(ac)/seed=$(seed)")
+            fs = filter(x-> !occursin("model", x),readdir(inpath, join=true))
+            for f in fs
+                d = load(f)
+                push!(outdf, row(d))
+            end
+            @info "Finished processing $inpath"
+        end
+    end
+end
+
+outdir = datadir("sgad_outputs/vector_params")
+mkdir(outdir)
+outf = joinpath(outdir, "images_$(datatype).bson")
+save(outf, Dict(:df => outdf))
+
+# mvtec
+datatype = "leave-one-in"
+mainpath = datadir("experiments/images_$(datatype)/$(model)")
+
+outdf = DataFrame(
+    :modelname => [],
+    :init_seed => [],
+    :dataset => [],
+    :anomaly_class => [],
+    :seed => [],
+    :weights_texture => [],
+    :detach_mask => []
+    )
+
+
+for dataset in ["wood", "grid", "transistor", "pill", "metal_nut", "capsule", "bottle"]
+    for ac in 1:1
+        for seed in 1:5
             inpath = joinpath(mainpath, "$(dataset)/ac=$(ac)/seed=$(seed)")
             fs = filter(x-> !occursin("model", x),readdir(inpath, join=true))
             for f in fs
