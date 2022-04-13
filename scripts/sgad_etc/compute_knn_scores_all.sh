83708c5344ebca67892108591bba7e2d311de6ab
Vít Škvára <vit.skvara@gmail.com>
2022-04-13 22:12:33 +0200

    knn scores per class

--- scripts/sgad_etc/compute_knn_scores_all.sh
+++ scripts/sgad_etc/compute_knn_scores_all.sh
@@ -2,5 +2,5 @@
 # this runs the computation for all datasets and score types at once
 FORCE=$1
 
-./compute_knn_scores_parallel.sh ../experiments_images/datasets_images_color.txt leave-one-in $FORCE
+#./compute_knn_scores_parallel.sh ../experiments_images/datasets_images_color.txt leave-one-in $FORCE
 ./compute_knn_scores_parallel.sh ../mvtec_ad/categories_sgvae.txt mvtec $FORCE
