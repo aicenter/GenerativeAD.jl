f5d5e16bf650eca62effeb2a93b4c5a923fa7d5b
Vít Škvára <vit.skvara@gmail.com>
2022-05-05 13:20:46 +0200

    gather cleanup

--- scripts/sgad_etc/gather_alpha_scores_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-P_NEGATIVE=$1
-VALAUC=$2
-
-./gather_alpha_scores_parallel.sh ../experiments_images/datasets_images_color.txt leave-one-in 10 ${P_NEGATIVE} $VALAUC
-./gather_alpha_scores_parallel.sh ../mvtec_ad/categories_sgvae.txt mvtec 1 ${P_NEGATIVE} $VALAUC
--- scripts/sgad_etc/regather_alpha_scores_all.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-./regather_alpha_scores_parallel.sh ../experiments_images/datasets_images_color.txt leave-one-in 10
-./regather_alpha_scores_parallel.sh ../mvtec_ad/categories_sgvae.txt mvtec 1
