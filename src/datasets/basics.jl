ebf417fc8ae56ee8db40672b5871052fae5bcbf8
Vít Škvára <vit.skvara@gmail.com>
2022-05-23 21:54:16 +0200

    alpha score

--- src/datasets/basics.jl
+++ src/datasets/basics.jl
@@ -160,12 +160,10 @@ function split_multifactor_data(anomaly_factors, train_class, scores_orig, mf_sc
     # construct the normal and anomalous datasets
     ainds = map(i->mf_labels[i,:] .!= train_class, anomaly_factors)
     ainds = map(is -> reduce(|, is), zip(ainds...))
-    if ndims(a_scores) == 1
-        a_scores = mf_scores[ainds]
-        n_scores = mf_scores[.!ainds]
-    elseif ndims(a_scores) == 2
-        a_scores = mf_scores[:,ainds]
-        n_scores = mf_scores[:,.!ainds]
+    a_scores, n_scores = if ndims(mf_scores) == 1
+         mf_scores[ainds], mf_scores[.!ainds]
+    elseif ndims(mf_scores) == 2
+        mf_scores[:,ainds], mf_scores[:,.!ainds]
     else
         error("this only works for 1D and 2D arrays")
     end
