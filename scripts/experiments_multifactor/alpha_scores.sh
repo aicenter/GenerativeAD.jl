0b5efc92b8940be2089bc0065f614fbb256ed2ff
Vít Škvára <vit.skvara@gmail.com>
2022-05-24 11:09:05 +0200

    alpha scores script

---
+++ scripts/experiments_multifactor/alpha_scores.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1 --ntasks-per-node=2 --cpus-per-task=2
+#SBATCH --partition=cpu
+#SBATCH --mem=20G
+
+MODELNAME=$1
+DATASET=$2
+LATENT_SCORE=$3
+AC=$4
+METHOD=$5
+AF1=$6
+AF2=$7
+AF3=$8
+
+module load Julia/1.5.3-linux-x86_64
+module load Python/3.9.6-GCCcore-11.2.0
+
+source ${HOME}/sgad-env/bin/activate
+export PYTHON="${HOME}/sgad-env/bin/python"
+julia --project -e 'using Pkg; Pkg.build("PyCall"); @info("SETUP DONE")'
+
+julia ./alpha_scores.jl ${MODELNAME} ${DATASET} ${LATENT_SCORE} $AC $METHOD -f --anomaly_factors $AF1 $AF2 $AF3
