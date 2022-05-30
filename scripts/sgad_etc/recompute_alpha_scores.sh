6feac8d139fbce2bb90cdaec0e94c9f17f19058b
Vít Škvára <vit.skvara@gmail.com>
2022-05-30 22:11:48 +0200

    robreg improvements

--- scripts/sgad_etc/recompute_alpha_scores.sh
+++ scripts/sgad_etc/recompute_alpha_scores.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --time=4:00:00
+#SBATCH --time=24:00:00
 #SBATCH --nodes=1 --ntasks-per-node=2 --cpus-per-task=2
-#SBATCH --partition=cpufast
+#SBATCH --partition=cpu
 #SBATCH --mem=30G
 
 DATASET=$1
