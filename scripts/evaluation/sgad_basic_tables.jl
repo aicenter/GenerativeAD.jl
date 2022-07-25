374a7c902d976d3ecc13a0a38b7cf67b5cc3d1e6
Vít Škvára <vit.skvara@gmail.com>
2022-07-25 08:09:43 +0200

    vaegan alias

--- scripts/evaluation/sgad_basic_tables.jl
+++ scripts/evaluation/sgad_basic_tables.jl
@@ -21,7 +21,7 @@ include("./utils/ranks.jl")
 outdir = "result_tables"
 
 sgad_models = ["DeepSVDD", "fAnoGAN", "fmgan", "vae", "cgn", "sgvae", "vaegan"]
-sgad_models_alpha = ["DeepSVDD", "fAnoGAN", "fmgan", "vae", "cgn", "sgvae", , "vaegan", "sgvae_alpha"]
+sgad_models_alpha = ["DeepSVDD", "fAnoGAN", "fmgan", "vae", "cgn", "sgvae", "vaegan", "sgvae_alpha"]
 sgad_models_alias = [MODEL_ALIAS[n] for n in sgad_models_alpha]
 
 TARGET_DATASETS = Set(["cifar10", "svhn2", "wmnist", "coco"])
