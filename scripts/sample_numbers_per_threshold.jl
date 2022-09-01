ac7c191ac77b17712d13ec129feaf13bb38b23c5
Vít Škvára <vit.skvara@gmail.com>
2022-09-01 08:10:22 +0200

    samples per threshold

--- scripts/sample_numbers_per_threshold.jl
+++ scripts/sample_numbers_per_threshold.jl
@@ -2,9 +2,11 @@ using DrWatson
 @quickactivate
 using GenerativeAD
 include("./supervised_comparison/utils.jl")
+using CSV
 using DataFrames
 using GenerativeAD.Evaluation: _subsample_data
 
+outf = datadir("evaluation/sample_numbers_per_threshold.csv")
 datasets = ["CIFAR10", "SVHN2", "cocoplaces", "wildlife_MNIST"]
 seed = 1
 ac = 1
@@ -13,18 +15,18 @@ ps = [NaN, 100.0, 50.0, 20.0, 10.0, 5.0, 2.0, 1.0, 0.5, 0.2, 0.1] ./ 100
 cols = 	[:full, :n100, :n50, :n20, :n10, :n5, :n2, :n1, :n05, :n02, :n01]
 outdf = DataFrame(
 	:dataset => String[],
-	:nn => Int[],
-	:full => Int[],
-	:n100 => Int[],
-	:n50 => Int[],
-	:n20 => Int[],
-	:n10 => Int[],
-	:n5 => Int[],
-	:n2 => Int[],
-	:n1 => Int[],
-	:n05 => Int[],
-	:n02 => Int[],
-	:n01 => Int[],
+	:nn => Any[],
+	:full => Any[],
+	:n100 => Any[],
+	:n50 => Any[],
+	:n20 => Any[],
+	:n10 => Any[],
+	:n5 => Any[],
+	:n2 => Any[],
+	:n1 => Any[],
+	:n05 => Any[],
+	:n02 => Any[],
+	:n01 => Any[],
 	)
 
 for dataset in datasets
@@ -43,27 +45,11 @@ for dataset in datasets
 	push!(outdf, r)
 end
 
-mvtecdf = DataFrame(
-	:dataset => String[],
-	:nn => Int[],
-	:full => Int[],
-	:n100 => Int[],
-	:n50 => Int[],
-	:n20 => Int[],
-	:n10 => Int[],
-	:n5 => Int[],
-	:n2 => Int[],
-	:n1 => Int[],
-	:n05 => Int[],
-	:n02 => Int[],
-	:n01 => Int[],
-	)
-
 categories = ["bottle",  "capsule",  "metal_nut", "pill", "transistor"]
-for cat in categories
+for category in categories
 	(tr_x, tr_y), (val_x, val_y), (tst_x, tst_y) = 
-		GenerativeAD.load_data("MVTec-AD", seed=seed, category=cat, img_size=128)
-	r = [cat, Int(length(val_y)-sum(val_y))]
+		GenerativeAD.load_data("MVTec-AD", seed=seed, category=category, img_size=128)
+	r = [category, Int(length(val_y)-sum(val_y))]
 	for p in ps
 		if isnan(p)
 			push!(r, Int(sum(val_y)))
@@ -72,7 +58,7 @@ for cat in categories
 				sub_x, sub_y = _subsample_data(p, 1.0, val_y, val_x)
 				push!(r, Int(sum(sub_y)))
 			catch e
-				if isa(DomainError, e)
+				if isa(e, DomainError)
 					push!(r, NaN)
 				else
 					rethrow(e)
@@ -82,3 +68,5 @@ for cat in categories
 	end
 	push!(outdf, r)
 end
+
+CSV.write(outf, outdf)
