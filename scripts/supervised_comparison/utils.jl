db5de8acdd06db6149e3119f4c65d7192004e1d2
Vít Škvára <vit.skvara@gmail.com>
2022-09-05 11:26:47 +0200

    alpha scores for supervised experiment

--- scripts/supervised_comparison/utils.jl
+++ scripts/supervised_comparison/utils.jl
@@ -151,7 +151,7 @@ function get_latent_file(_params, lfs)
 	else
 		_lfs
 	end
-	if length(_lfs) >= 1
+	if length(_lfs) > 1
 		error("something wrong when processing $(_params)")
 	elseif length(_lfs) == 0
 		return
