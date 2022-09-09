3e19d00f420829e7eec62cd347407995df085602
Vít Škvára <vit.skvara@gmail.com>
2022-09-09 22:02:15 +0200

    knn scores randomized

--- scripts/sgad_etc/compute_knn_scores_randomized.jl
+++ scripts/sgad_etc/compute_knn_scores_randomized.jl
@@ -28,6 +28,7 @@ max_seed = (datatype == "mvtec") ? 5 : 1
 ks = (datatype == "mvtec") ? collect(1:4:151) : vcat([1, 31, 61], collect(101:100:2001))
 ks = (dataset == "cocoplaces") ? collect(1:10:201) : ks
 ac = anomaly_class
+max_tries = 20
 
 while true # run this over and over until the job time limit is exhausted
     for seed in 1:max_seed
@@ -42,12 +43,13 @@ while true # run this over and over until the job time limit is exhausted
         model_ids = map(x-> Meta.parse(split(split(x, "=")[2], ".")[1]), readdir(in_dir))
         for model_id in model_ids
             res = nothing
-            while isnothing(res) # the script might get stuck here
+            ntries = 1
+            while isnothing(res) && ntries <= max_tries # the script might get stuck here
                 k = sample(ks, 1)[1]
                 v = sample([:delta, :kappa, :gamma], 1)[1]
                 res = compute_knn_score(model_id, in_dir, k, v, out_dir, seed, ac, dataset, modelname; 
                     force=force)                    
-                end
+                ntries += 1
             end
         end
     end
