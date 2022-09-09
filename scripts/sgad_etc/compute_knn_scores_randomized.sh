d67d476849a820899e2b3f28e5462f04b8a84855
Vít Škvára <vit.skvara@gmail.com>
2022-09-09 22:02:57 +0200

    knn randomized

---
+++ scripts/sgad_etc/compute_knn_scores_randomized.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1 --ntasks-per-node=2 --cpus-per-task=2
+#SBATCH --mem=20G
+
+MODEL=$1
+DATASET=$2
+DATATYPE=$3
+AC=$4
+FORCE=$5
+
+module load Julia/1.5.3-linux-x86_64
+module load Python/3.9.6-GCCcore-11.2.0
+
+source ${HOME}/sgad-env/bin/activate
+export PYTHON="${HOME}/sgad-env/bin/python"
+julia --project -e 'using Pkg; Pkg.build("PyCall"); @info("SETUP DONE")'
+
+julia ./compute_knn_scores_randomized.jl $MODEL ${DATASET} ${DATATYPE} $AC $FORCE
---
+++ scripts/sgad_etc/compute_knn_scores_randomized_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# this runs the computation for all datasets and score types at once
+
+MODEL=$1
+FORCE=$2
+
+for AC in {1..10}
+do
+	./compute_knn_scores_randomized_parallel.sh $MODEL ../experiments_images/datasets_images_color.txt leave-one-in $AC $FORCE
+done
