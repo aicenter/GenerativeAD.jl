400491667a4fd71efbf315bcf9b3d30da84625fc
Vít Škvára <vit.skvara@gmail.com>
2022-11-11 13:15:04 +0100

    jacodeco

---
+++ scripts/jacodeco/jacodeco_py.jl
@@ -0,0 +1,75 @@
+# this is meant to compute the logDetJaco score (part of jacodeco) for the python sgad models
+using DrWatson
+@quickactivate
+using ArgParse
+using GenerativeAD
+import StatsBase: fit!, predict
+using BSON, FileIO
+
+include("../pyutils.jl")
+
+s = ArgParseSettings()
+@add_arg_table! s begin
+	"modelname"
+		default = "sgvaegan100"
+		arg_type = String
+		help = "model name"
+	"dataset"
+		default = "SVHN2"
+		arg_type = String
+		help = "dataset"
+	"anomaly_class"
+		default = 1
+		arg_type = Int
+		help = "which anomaly class to compute"
+	"--force"
+        action = :store_true
+        help = "force recomputing of scores"
+end
+parsed_args = parse_args(ARGS, s)
+@unpack dataset, modelname, anomaly_class, force = parsed_args
+method = "leave-one-in"
+ac = anomaly_class
+data = GenerativeAD.load_data(dataset, seed=1, anomaly_class_ind=ac, method=method)
+data = GenerativeAD.Datasets.normalize_data(data)
+mdirs = readdir(modelpath, join=true)
+device = "cuda"
+seed = 1
+
+modelpath = datadir("sgad_models/images_$(method)/$(modelname)/$(dataset)/ac=$(ac)/seed=$(seed)")
+outpath = datadir("jacodeco/images_$(method)/$(modelname)/$(dataset)/ac=$(ac)/seed=$(seed)")
+mkpath(outpath)
+
+function compute_log_jacodet_scores(md)
+	outf = joinpath(outpath, "$(basename(md)).bson")
+	if isfile(outf) && !force
+		@info "$outf present, skipping"
+	end
+
+	model = if occursin("sgvaegan", modelname)
+		GenerativeAD.Models.SGVAEGAN(load_sgvaegan_model(md, device))
+	else
+		throw("unknown model type $modelname")
+	end
+
+	score(x) = predict(model, x, score_type="log_jacodet", workers=4)
+
+	tr_scores, val_scores, tst_scores = score(data[1][1]), score(data[2][1]), score(data[3][1])
+	outd = Dict(
+		:tr_scores => tr_scores,
+		:val_scores => val_scores,
+		:tst_scores => tst_scores,
+		:model_id => Meta.parse(split(basename(md),"=")[2]), 
+		:dataset => dataset,
+		:modelname => modelname,
+		:anomaly_class => ac,
+		:seed => seed
+		)
+	save(outd, )
+	@info "saved $outf"
+	return outd
+end
+
+for md in mds
+	compute_log_jacodet_scores(md)
+end
---
+++ scripts/jacodeco/jacodeco_py.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --partition gpu
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node 1
+#SBATCH --mem-per-cpu 80G
+#SBATCH --time 24:00:00
+#SBATCH --job-name jacodeco
+#SBATCH --output ~/logs/jacodeco/%J.log
+
+MODEL=$1
+DATASET=$2
+AC=$3
+FORCE=$4
+
+module load Julia/1.5.3-linux-x86_64
+module load Python/3.9.6-GCCcore-11.2.0
+
+source ${HOME}/sgad-env/bin/activate
+export PYTHON="${HOME}/sgad-env/bin/python"
+julia --project -e 'using Pkg; Pkg.build("PyCall"); @info("SETUP DONE")'
+
+julia ./jacodeco_py.jl $MODEL $DATASET $AC $FORCE
+
